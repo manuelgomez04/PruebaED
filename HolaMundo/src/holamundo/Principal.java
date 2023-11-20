ff6fab5fd548f5539c25e2f72ecb309ee4605bcb
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2023-11-20 13:51:58 +0100

    Me quedau agusto

--- HolaMundo/src/holamundo/Principal.java
+++ HolaMundo/src/holamundo/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Probando probando probando probando");
+		System.out.println("Victor es to pesao que locura tío, encima es to tonto y no se entera de na el coleguita"
+				+ "a ver si espabila de una vez y se calla la boca mi niño x fin xq yo no puedo más ya con su gilipollez "
+				+ "es acojonante enserio loco. Más pesao que una vaca en brazos :)");
 
 	}
 
